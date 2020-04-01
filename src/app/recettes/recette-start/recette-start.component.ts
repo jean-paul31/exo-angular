f03cfe23451c0340b72ab491dab8050a57d52332
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-01 17:13:10 +0200

    last commit

---
+++ src/app/recettes/recette-start/recette-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recette-start',
+  templateUrl: './recette-start.component.html',
+  styleUrls: ['./recette-start.component.css']
+})
+export class RecetteStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
